e2a05c17b471aa57e2eadfd60d57836113dd8998
codepathi <65897679+codepathi@users.noreply.github.com>
2023-01-31 21:06:50 +0545

    ss
    ss

--- src/framework/basic-rest/utils/baseUrl.ts
+++ src/framework/basic-rest/utils/baseUrl.ts
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 const base = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_DEPLOYED_URL,
-  baseURL: "https://restrox-fullonline-backend-jstzp.ondigitalocean.app/restrox/api",
+  baseURL: "https://testbackend.restrox.co/restrox/api",
   timeout: 50000,
 });
-
 export default base;
 
 //  Test: 
